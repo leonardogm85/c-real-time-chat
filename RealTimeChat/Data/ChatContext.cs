799ea84f1695b681ba3de7896fbc705225e8308e
Leonardo Martins <leonardogm85@gmail.com>
2023-01-29 19:33:29 -0300

    Create project

---
+++ RealTimeChat/Data/ChatContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RealTimeChat.Data
+{
+    public sealed class ChatContext : DbContext
+    {
+        public ChatContext(DbContextOptions<ChatContext> options) : base(options)
+        {
+        }
+    }
+}
---
+++ RealTimeChat/Program.cs
@@ -0,0 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using RealTimeChat.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<ChatContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}");
+
+app.Run();
